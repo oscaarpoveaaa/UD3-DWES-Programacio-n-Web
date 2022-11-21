a3651d462fca997d15c40abb0fb881e0209547c5
Oscar Povea <oscaarpoveaa@gmail.com>
2022-11-21 20:09:57 +0100

    sigo

--- 001/001server.php
+++ 001/001server.php
@@ -1,5 +1,26 @@
+<!-- 001server.php: igual que el ejemplo visto en los apuntes, muestra los valores de 
+$_SERVER al ejecutar un script en tu ordenador.
+Prueba a pasarle parámetros por GET (y a no pasarle ninguno).
+Prepara un formulario (001post.html) que haga un envío por POST y compruébalo de 
+nuevo.
+Crea una página (001enlace.html) que tenga un enlace a 001server.php y comprueba 
+el valor de HTTP_REFERER. -->
+
 <?php 
 
+echo $_SERVER["PHP_SELF"]. "<br>";
+echo $_SERVER["SERVER_SOFTWARE"]. "<br>";
+echo $_SERVER["SERVER_NAME"]. "<br>";
+echo $_SERVER["REQUEST_METHOD"]. "<br>";
+echo $_SERVER["REQUEST_URI"]. "<br>";
+echo $_SERVER["QUERY_STRING"]. "<br>";
 
+$nombre = $_POST("nombre");
+$apellido = $_POST("apellido");
+$usuario = $_POST("usuario");
+
+echo("Nombre: " . $nombre);
+echo("Apellido: " . $apellido);
+echo("Usuario: " . $usuario);
 
 ?>
